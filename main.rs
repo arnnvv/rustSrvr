499213913c32a053bee4e4002590532918c2468e
arnnvv <arnav.900@protonmail.com>
2024-05-12 15:53:37 +0530

    readingREQ

--- main.rs
+++ main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     const HOST: &str = "127.0.0.1";
@@ -12,5 +14,13 @@ fn main() {
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
         println!("Connection established!");
+        handle_connection(_stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
